d60aa3269758140b7aba46e0ad91c1e83747041e
Nay Oo Kyaw <nayookyaw.nok@gmail.com>
2025-11-10 02:58:37 +1300

    integrate css with razor locally

---
+++ frontend-blazor/Constants/AppConstants.cs
@@ -0,0 +1,9 @@
+namespace Frontend.Constants;
+
+public static class AppConstants
+{
+    public static readonly int DEFAULT_ZOOM = 5;
+    public static readonly double[] DEFAULT_COORDINATE = new double[] { 174.7633, -36.8485 }; // Auckland coordinates
+    public static readonly int DEFAULT_PAGE = 1;
+    public static readonly int DEFAULT_PAGE_SIZE = 20;
+}
--- frontend-blazor/Services/SensorServiceApi/ISensorApiClient.cs
+++ frontend-blazor/Services/SensorServiceApi/ISensorApiClient.cs
@@ -4,5 +4,5 @@ namespace Frontend.Services.SensorServiceApi;
 
 public interface ISensorApiClient
 {
-    Task<PagedResult<SensorDto>> SearchSensorList(string? q, int page, int pageSize, CancellationToken ct = default);
+    Task<PagedResult<SensorDto>> SearchSensorList(string? searchText, int page, int pageSize, CancellationToken ct = default);
 }
